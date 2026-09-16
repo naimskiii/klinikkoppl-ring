9fc26f6ddd2bffdae9f69f79e021471c8110c583
naima <naimz0605@gmail.com>
2026-09-17 00:04:23 +0200

    dockerfile

--- Backend/Entity/obj/Debug/net10.0/Entity.AssemblyInfo.cs
+++ Backend/Entity/obj/Debug/net10.0/Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b22c12b26cbb59f07e2c67f4fc6febd20c18d11a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639199fcad8f9595620e464487d7cbb47499d411")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
