a5c533acf4db32a9cbf99cc1e1ec106c9f764092
naima <naimz0605@gmail.com>
2026-09-18 13:34:18 +0200

    update url

--- Backend/Entity/obj/Debug/net10.0/Entity.AssemblyInfo.cs
+++ Backend/Entity/obj/Debug/net10.0/Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e455832f5c276b4f65667d67567740cbe98d1b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c61cb303dac5c560f452ee92948913f893f1ef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
